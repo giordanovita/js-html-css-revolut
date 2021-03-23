32ae6921db8199210bbf89e3bacb8978447d2213
giordanovita <giordanovitag@libero.it>
2021-03-23 18:58:52 +0100

    ex complete

--- script.js
+++ script.js
@@ -17,27 +17,39 @@ function dropper(){
         $('.plans-list').show();
     }) 
 
+    $('.features').hover(function(){
+            
+        $('.features-list').show();
+    }) 
+
 
  
     $(".help").hover(function(){
         $(".help-list").css("visibility","visible"); 
       });
-      $(".about, .plans").hover(function() {
+      $(".about, .plans, .features").hover(function() {
           $(".help-list").css("visibility","hidden"); 
       });
 
       $(".about").hover(function(){
         $(".about-list").css("visibility","visible"); 
       });
-     $(".help, .plans").hover(function() {
+     $(".help, .plans, .features").hover(function() {
           $(".about-list").css("visibility","hidden"); 
       });
 
       $(".plans").hover(function(){
         $(".plans-list").css("visibility","visible"); 
       });
-     $(".help, .about").hover(function() {
+     $(".help, .about, .features").hover(function() {
           $(".plans-list").css("visibility","hidden"); 
+      }); 
+
+      $(".features").hover(function(){
+        $(".features-list").css("visibility","visible"); 
+      });
+     $(".help, .about, .plans").hover(function() {
+          $(".features-list").css("visibility","hidden"); 
       }); 
 
       
@@ -48,7 +60,7 @@ function closer(){
 
     $('html').click(function(){
             
-        $('.help-list, .about-list, .plans-list ').hide();
+        $('.help-list, .about-list, .plans-list, .features-list ').hide();
     })
 }
 
